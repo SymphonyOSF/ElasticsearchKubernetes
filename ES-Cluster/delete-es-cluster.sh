0dd10c5b1b6de5a2207ef9e5201dce8f973d8211
Luis Plazas <luis.plazas@symphony.com>
2019-11-18 10:39:43 -0800

    Moved ES-Cluster files to an outer directory.

---
+++ ES-Cluster/delete-es-cluster.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+CLUSTER_NAME=$1
+
+if [ ! -f "./${CLUSTER_NAME}" ]; then
+    echo "File not found!"
+    echo "You must pass as a parameter the yaml file with the name of your cluster in the current directory."
+    echo "E.g. ./delete-es-cluster.sh symsearch-1d2f.yml"
+    exit 1;
+fi
+
+# Delete the secret containing the SSL certificate
+kubectl delete secret ${CLUSTER_NAME}
+
+#Remove elasticsearch and kibana deployments/sts based on the name given as parameter.
+kubectl delete -f "./${CLUSTER_NAME}.yml"
+
+if [[ $0 == 0 ]]; then
+    rm -rf "./${CLUSTER_NAME}"
+fi;
