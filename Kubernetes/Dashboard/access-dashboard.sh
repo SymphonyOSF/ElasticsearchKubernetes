2feb3261b9ce8f7e43ff2cd23c57a8d613b60f6f
Luis Plazas <luis.plazas@symphony.com>
2019-11-18 16:14:09 -0800

    Change dashboard user logic.

--- Kubernetes/Dashboard/access-dashboard.sh
+++ Kubernetes/Dashboard/access-dashboard.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep dashboard-user | awk '{print $1}')
 (sleep 3; open http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/)&
 kubectl proxy
