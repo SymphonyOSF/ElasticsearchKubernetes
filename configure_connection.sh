2efd25e4cb62e563c84365b5a4481682a11e8ac1
Luis Plazas <luis.plazas@symphony.com>
2019-11-26 12:47:36 -0800

    Update configure connection file for ES deployments.

--- configure_connection.sh
+++ configure_connection.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env bash
 # Exit when any command fails
 set -e
+
+# Trap always the last command executed
+trap 'previous_command=$this_command; this_command=$BASH_COMMAND' DEBUG
+
+#trap all errors
+trap 'catch $?' EXIT
+catch() {
+  if [[ "$1" != "0" ]]; then
+    echo "${bold}\"${previous_command}\" command on failed with exit code $1.${normal}"
+  fi
+}
+
 # Bold and normal font modifiers
 bold=$(tput bold)
 normal=$(tput sgr0)
-# Trap all error calls and print error message.
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-trap 'echo "${bold}\"${last_command}\" command failed with exit code $?.${normal}"' EXIT
-
 
 #Configure aws cli
-echo "${bold}Important: ${normal}When prompted, enter ${bold}json ${normal}as default output format"
 aws configure
-
 
 read -r -p "Enter EKS cluster name [As configured on terraform]: " CLUSTER_NAME
 read -r -p "Enter the role ARN [Obtained from the terraform output]: " ROLE_ARN
 #Configure .kubeconfig for kubectl to point to the cluster.
 aws eks update-kubeconfig --name ${CLUSTER_NAME} --role-arn ${ROLE_ARN}
 
+#Verify that the authentication was successful
 kubectl auth can-i '*' '*'
 
 echo "Successfully configured kubectl"
