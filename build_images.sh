765ef4b9982627ac9142de2ddfa9cbea9654754c
Luis Plazas <luis.plazas@symphony.com>
2019-11-27 09:00:43 -0800

    Change docker images to use new base image. Added automatic image building script.

---
+++ build_images.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#Build base image
+docker build . -f ./Dockerfile.deploy-base -t luisplazas/deploy-base
+docker push luisplazas/deploy-base
+
+#Build eks deployment image
+docker build . -f ./Dockerfile.eks-deploy -t luisplazas/eks-deploy
+docker push luisplazas/eks-deploy
+
+#Build elastic deployment image
+docker build . -f ./Dockerfile.elastic-deploy -t luisplazas/elastic-deploy
+docker push luisplazas/elastic-deploy
