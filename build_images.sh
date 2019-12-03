bc16f42134f0f073b6c29906702923e4987b5cbc
Luis Plazas <luis.plazas@symphony.com>
2019-12-03 12:24:20 -0800

    Update image build file.

--- build_images.sh
+++ build_images.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
+PUSH_IMAGE=false
+if [[ $1 == "-push" ]]; then
+  PUSH_IMAGE=true
+fi
+
 #Build base image
 docker build . -f ./Dockerfile.deploy-base -t luisplazas/deploy-base
-#docker push luisplazas/deploy-base
+if [[ $PUSH_IMAGE == true ]]; then
+ docker push luisplazas/deploy-base
+fi
 
 #Build eks deployment image
 docker build . -f ./Dockerfile.eks-deploy -t luisplazas/eks-deploy
-#docker push luisplazas/eks-deploy
+if [[ $PUSH_IMAGE == true ]]; then
+  docker push luisplazas/eks-deploy
+fi
 
 #Build elastic deployment image
 docker build . -f ./Dockerfile.elastic-deploy -t luisplazas/elastic-deploy
-#docker push luisplazas/elastic-deploy
+if [[ $PUSH_IMAGE == true ]]; then
+  docker push luisplazas/elastic-deploy
+fi
 
+#Build elasticsearch
 docker build . -f ./Dockerfile.elastic -t elastic:7.4.2
-
-#docker push luisplazas/elastic:7.4.2
+if [[ $PUSH_IMAGE == true ]]; then
+  docker push luisplazas/elastic:7.4.2
+fi
