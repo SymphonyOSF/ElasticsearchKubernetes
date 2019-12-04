39ea2f8afd95b9ba9adaa7b9ce7d9eb690a61297
Luis Plazas <luis.plazas@symphony.com>
2019-12-04 15:06:27 -0800

    Update image build script with new image.

--- build_images.sh
+++ build_images.sh
@@ -4,22 +4,9 @@ if [[ $1 == "-push" ]]; then
   PUSH_IMAGE=true
 fi
 
-#Build base image
-docker build . -f ./Dockerfile.deploy-base -t luisplazas/deploy-base
+docker build . -f ./Dockerfile.deployment -t luisplazas/deployment:latest
 if [[ $PUSH_IMAGE == true ]]; then
- docker push luisplazas/deploy-base
-fi
-
-#Build eks deployment image
-docker build . -f ./Dockerfile.eks-deploy -t luisplazas/eks-deploy
-if [[ $PUSH_IMAGE == true ]]; then
-  docker push luisplazas/eks-deploy
-fi
-
-#Build elastic deployment image
-docker build . -f ./Dockerfile.elastic-deploy -t luisplazas/elastic-deploy
-if [[ $PUSH_IMAGE == true ]]; then
-  docker push luisplazas/elastic-deploy
+ docker push luisplazas/deployment
 fi
 
 #Build elasticsearch
