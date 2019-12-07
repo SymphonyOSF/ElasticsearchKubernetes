f6493f8202bdcb787900ebfaf644ca1004eaeed9
Luis Plazas <luis.plazas@symphony.com>
2019-12-06 18:53:16 -0800

    Add no input functionality to delete-es-cluster.sh script. Add verification to delete-es-cluster.sh before deleting clusters.

--- ESClusterDeployment/delete-es-cluster.sh
+++ ESClusterDeployment/delete-es-cluster.sh
@@ -18,25 +18,48 @@ NORMAL=$(tput sgr0)
 #Test kubectl connection
 kubectl auth can-i '*' '*' >/dev/null
 
-#Get cluster name
-read -r -p "${BOLD}Enter cluster name to delete:${NORMAL} " CLUSTER_NAME
+#$1: Cluster name to delete
+function delete_cluster() {
+    CLUSTER_NAME=$1
+    set +e
+    kubectl get Elasticsearch/${CLUSTER_NAME} >/dev/null 2>&1
+    if [[ $? != 0 ]]; then
+        echo "${BOLD}************* ERROR ************* ${NORMAL}"
+        echo "The cluster specified doesn't exist"
+        echo "This is the list of existing Elastic clusters:"
+        kubectl get Elasticsearch -o=custom-columns=CLUSTER_NAME:.metadata.name
+        echo "${BOLD}************* ERROR ************* ${NORMAL}"
+        exit 0;
+    fi
+    set -e
 
-set +e
-kubectl get Elasticsearch/${CLUSTER_NAME} >/dev/null 2>&1
-if [[ $? != 0 ]]; then
-    echo "${BOLD}************* ERROR ************* ${NORMAL}"
-    echo "The cluster specified doesn't exist"
-    echo "This is the list of existing Elastic clusters:"
-    kubectl get Elasticsearch -o=custom-columns=CLUSTER_NAME:.metadata.name
-    echo "${BOLD}************* ERROR ************* ${NORMAL}"
-    exit 0;
+    #Remove Elasticsearch
+    kubectl delete Elasticsearch/${CLUSTER_NAME}
+
+    #Remove Kibana
+    kubectl delete Kibana/kibana-${CLUSTER_NAME}
+
+    echo "Successfully deleted ${CLUSTER_NAME}"
+}
+
+
+if [[ $1 != "-y" || -z "$2" ]]; then
+    read -r -p "${BOLD}Enter cluster name to delete:${NORMAL} " CLUSTER_NAME
+    read -r -p "Are you sure you want to delete cluster ${CLUSTER_NAME}? [Y/n] " input
+
+    case $input in
+        [yY][eE][sS]|[yY])
+            delete_cluster ${CLUSTER_NAME}
+        ;;
+        [nN][oO]|[nN])
+            echo "Cancelling execution."
+        ;;
+        *)
+     echo "Invalid input..."
+     exit 1
+     ;;
+    esac
+else
+    CLUSTER_NAME=$2
+    delete_cluster ${CLUSTER_NAME}
 fi
-set -e
-
-#Remove Elasticsearch
-kubectl delete Elasticsearch/${CLUSTER_NAME}
-
-#Remove Kibana
-kubectl delete Kibana/kibana-${CLUSTER_NAME}
-
-echo "Successfully deleted ${CLUSTER_NAME}"
