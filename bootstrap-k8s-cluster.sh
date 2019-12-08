03a511e1971d636f2624aac47729508112af9ef1
Luis Plazas <luis.plazas@symphony.com>
2019-12-07 23:21:45 -0800

    Update bootstrap-k8s-cluster.sh to use the terraform output of the directory it is being called from.

--- bootstrap-k8s-cluster.sh
+++ bootstrap-k8s-cluster.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 set -e
+
+#Get terraform output in current directory
+#TT;
+#terraform output -json eks_cluster_output | jq -r .config_map_aws_auth >> ${TT}
+SERVICE_INSTANCE_TYPE=$(terraform output -json eks_cluster_output | jq -r .service_instance_type)
+CLUSTER_NAME=$(terraform output -json eks_cluster_output | jq -r .cluster_name)
+
 #CD into the directory containing this script
-cd "${0%/*}"
+SCRIPT_DIR="${0%/*}"
 
 # Bold and normal font modifiers
 bold=$(tput bold)
@@ -18,22 +25,19 @@ catch() {
   fi
 }
 
-#Regenerate the config_map file for K8S to discover worker nodes
 #Get cluster_name and service_instance_type from terraform output.
-true | tee ./config_map_aws_auth.yml
-terraform output -json eks_cluster_output | jq -r .config_map_aws_auth >> ./Kubernetes/config_map_aws_auth.yml
-SERVICE_INSTANCE_TYPE=$(terraform output -json eks_cluster_output | jq -r .service_instance_type)
-CLUSTER_NAME=$(terraform output -json eks_cluster_output | jq -r .cluster_name)
-cd ./Kubernetes
+#cd ${SCRIPT_DIR}/Kubernetes
 
 #Change kubectl authentication conf to match the K8S master
 aws eks update-kubeconfig --name "${CLUSTER_NAME}"
 
 #Apply config_map file to K8S, this will allow the control plane discover worker nodes
-kubectl apply -f ./config_map_aws_auth.yml
+terraform output -json eks_cluster_output \
+    | jq -r .config_map_aws_auth \
+    | kubectl apply -f -
 
 #Install AWS auto-scaler. Replace cluster_name and service_instance_type
-sed "s/NEW_CLUSTER_NAME/${CLUSTER_NAME}/g" cluster-autoscaler-autodiscover.yaml \
+sed "s/NEW_CLUSTER_NAME/${CLUSTER_NAME}/g" ${SCRIPT_DIR}/Kubernetes/cluster-autoscaler-autodiscover.yaml \
     | sed -E "s/SERVICE_INSTANCE_TYPE/${SERVICE_INSTANCE_TYPE}/g" \
     | kubectl apply -f -
 
@@ -45,16 +49,16 @@ kubectl -n kube-system annotate --overwrite deployment.apps/cluster-autoscaler c
 kubectl -n kube-system set image deployment.apps/cluster-autoscaler cluster-autoscaler=k8s.gcr.io/cluster-autoscaler:v1.14.6
 
 #Bring up DNS mapping service
-#kubectl apply -f ./DNS/external_dns.yaml
+#kubectl apply -f ${SCRIPT_DIR}/DNS/external_dns.yaml
 
 #---------- Kubernetes Dashboard ------------
 # Run the Kubernetes Dashboard, 2.0.0-beta1
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta1/aio/deploy/recommended.yaml
 # Create dashboard admin user and attach admin role to it
-kubectl apply -f ./Dashboard/dashboard-user.yaml
+kubectl apply -f ${SCRIPT_DIR}/Kubernetes/Dashboard/dashboard-user.yaml
 
 #-------- /End Kubernetes Dashboard ----------
 
 #Configure the elastic operator. Replace instance_type and service_instance_type
-sed "s/SERVICE_INSTANCE_TYPE/${SERVICE_INSTANCE_TYPE}/g" all-in-one.yaml \
+sed "s/SERVICE_INSTANCE_TYPE/${SERVICE_INSTANCE_TYPE}/g" ${SCRIPT_DIR}/Kubernetes/all-in-one.yaml \
     | kubectl apply -f -
