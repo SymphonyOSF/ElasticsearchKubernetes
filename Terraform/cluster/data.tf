00569e8d3a1d44998125ecf7d805105cef7d0a8c
Luis Plazas <luis.plazas@symphony.com>
2019-12-16 13:26:08 -0800

    Update cluster module to not create ssh security group if not necessary.

--- Terraform/cluster/data.tf
+++ Terraform/cluster/data.tf
@@ -1,3 +1,4 @@
 locals {
-  ssh_sg_id_list = compact(concat([aws_security_group.node_ssh.id], var.ssh_sg_id_list))
+  office_ssh_sg_id = aws_security_group.node_ssh[0] != null ? aws_security_group.node_ssh[0].id : null
+  ssh_sg_id_list = compact(concat([local.office_ssh_sg_id], var.ssh_sg_id_list))
 }
